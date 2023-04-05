94f2f67863e60efa035a84a85875325917f06be5
Kasra071 <bitcoinsamani@yahoo.com>
2023-04-05 19:02:57 +0430

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+PageLoaded()
+function PageLoaded()
+{
+    let loader = document.getElementById('loader');
+    let content = document.getElementById('content')
+    loader.style.display = 'none'
+    content.style.display = "block"
+
+}
+
