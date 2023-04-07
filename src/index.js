d4b0b59d599c5a5e8d1a76314da9cc96836c5d02
Kasra071 <bitcoinsamani@yahoo.com>
2023-04-07 20:12:42 +0430

    contact and responsive remaining

--- src/index.js
+++ src/index.js
@@ -41,3 +41,43 @@ function ChangeLang(lang){
             break;
     }
 }
+
+ChangeTabServices(document.getElementById('Front-end-development'))
+var currentLeftOffset = 0;
+var animCount = 0;
+function ChangeTabServices(element){
+    let highlighter = document.querySelector('.tab-highlighter')
+    let StyleTag = document.getElementById('HighlightMoverAnimation')
+    let tab = element.getAttribute('tab-data')
+    let LeftCount;
+    switch(tab){
+        case 'FED':
+            LeftCount = 0
+            break;
+        case 'SPD':
+            LeftCount = 25
+            break;
+        case 'WRD':
+            LeftCount = 50
+            break;
+        case 'RD':
+            LeftCount = 75
+            break;
+    }
+
+    StyleTag.innerHTML = `
+        @keyframes Movehighlighter-${animCount} {
+            0%{left: ${currentLeftOffset}%;}
+            100%{left: ${LeftCount}%;}
+        }
+    `
+    currentLeftOffset = LeftCount
+    highlighter.style.animation = "none";
+    highlighter.style.animation = `Movehighlighter-${animCount} 0.2s ease-in-out forwards`
+    document.querySelectorAll('.ServiceShower').forEach(elem =>{
+        elem.style.display = 'none'
+    })
+    document.getElementById(tab).style.display = 'block'
+    animCount++;
+
+}
