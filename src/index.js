ae7ae38c7b0510b417514b8cf6a49d0f872a45cd
Kasra071 <bitcoinsamani@yahoo.com>
2023-04-07 08:24:07 +0430

    added about me and the portfolio (still got work to do)

--- src/index.js
+++ src/index.js
@@ -6,5 +6,38 @@ function PageLoaded()
     loader.style.display = 'none'
     content.style.display = "block"
 
+    document.querySelector('.overviewContainer').style.height = window.innerHeight + 'px';
 }
 
+window.addEventListener('resize',()=>{
+    document.querySelector('.overviewContainer').style.height = window.innerHeight + 'px';
+})
+
+function SideMenuOnMouseOver(elem){
+    elem.querySelector('a').style.animation = ""
+    elem.querySelector('a').style.display = "flex"
+}
+
+
+function SideMenuOnMouseLeave(elem){
+    elem.querySelector('a').style.animation = "sideAnimHide 0.2s forwards"
+    setTimeout(()=>{
+        elem.querySelector('a').style.display = "none"
+    },200)
+}
+
+function ChangeLangMenuShowUp()
+{
+    document.querySelector('.LanguageSelect div').classList.toggle('ShowingOption')
+}
+
+function ChangeLang(lang){
+    switch(lang){
+        case 'fa':
+            location.href = window.location.origin + '/fa/'
+            break;
+        case 'en':
+            location.href = window.location.origin + '/en/'
+            break;
+    }
+}
