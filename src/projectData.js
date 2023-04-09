5a1cf5c42cfc523c4a657928cd407c15701b4265
Kasra071 <bitcoinsamani@yahoo.com>
2023-04-10 01:49:11 +0430

    added persian page

---
+++ src/projectData.js
@@ -0,0 +1,31 @@
+var clientProjects = [
+    {
+        Name : 'mana-georgia.com',
+        img : 'dl/projects/mana/1.png',
+        desc : 'this website was built for a marriage company based in georgia. you can check their website for more information if u want',
+        builtWith : [
+            'HTML','CSS','JS'
+        ],
+        github : null,
+        url : 'https://mana-georgia.com',
+
+        NameFa : 'mana-georgia.com',
+        descFa : 'این وبسایت برای یک موسسه ازدواج واقع در گرجستان ساخته شده. برای دیدن کامل صفحات و اطلاعات بیشتر میتونین به سایتشون سر بزنین',
+    }
+]
+
+var PersonalProjects = [
+    {
+        Name : 'Numbers Conversion',
+        img : 'dl/projects/n_c/1.png',
+        desc : 'in this page you can convert numbers from any base to any base. all bases from base 2 to base 36 are supported. just choose the bases , and start converting ! ',
+        builtWith : [
+            'HTML','CSS','JS'
+        ],
+        github : 'https://github.com/Kasra071/Numbers_Conversion',
+        url : 'https://Numbers_Conversion.kasra071.com',
+
+        NameFa : 'تبدیل کننده اعداد',
+        descFa : 'در این صفحه میتوانید سیستم های عددی مختلف را به یکدیگر تبدیل کنید. از سیستم 2 تایی تا سیستم 36 تایی. کافیه سیستم ها و اعدادتون رو وارد کنید و تبدیل کردن رو شروع کنید !',
+    }
+]
