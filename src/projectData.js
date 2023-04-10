638a375544a0b41b119f8781cb58f44e3df6d0c8
Kasra071 <bitcoinsamani@yahoo.com>
2023-04-11 01:25:22 +0430

    another bug fixed

--- src/projectData.js
+++ src/projectData.js
@@ -1,7 +1,7 @@
 var clientProjects = [
     {
         Name : 'mana-georgia.com',
-        img : 'dl/projects/mana/1.png',
+        img : 'dl_k071/projects/mana/1.png',
         desc : 'this website was built for a marriage company based in georgia. you can check their website for more information if u want',
         builtWith : [
             'HTML','CSS','JS'
@@ -14,13 +14,13 @@ var clientProjects = [
     },    
     {
         Name : 'USA interactive map',
-        img : 'dl/projects/USA_MAP/1.png',
+        img : 'dl_k071/projects/USA_MAP/1.png',
         desc : "this simple page was built for a client's school project. its a interactive usa map where you can make lists of states and when you click on each list id on the side buttons, they will be highlighted on the map.",
         builtWith : [
             'HTML','CSS','JS'
         ],
         github : 'https://github.com/Kasra071/USA-interactive-map',
-        url : 'https://USA_interactive_map.Kasra071.com',
+        url : 'https://USA-interactive-map.Kasra071.com',
 
         NameFa : 'نقشه آمریکای تعاملی',
         descFa : 'این صفحه وب ساده برای پروژه مدرسه ای یک مشتری ساخته شده. یک مپ آمریکاست که میتونین توش یه لیست از ایالتا درست کنید و هروقت روی آیدی اون لیست کلیک کنید، ایالت های داخل اون لیست، روی مپ رنگ میشن و میتونین ببینیدشون'
@@ -30,13 +30,13 @@ var clientProjects = [
 var PersonalProjects = [
     {
         Name : 'Numbers Conversion',
-        img : 'dl/projects/n_c/1.png',
+        img : 'dl_k071/projects/n_c/1.png',
         desc : 'in this page you can convert numbers from any base to any base. all bases from base 2 to base 36 are supported. just choose the bases , and start converting ! ',
         builtWith : [
             'HTML','CSS','JS'
         ],
         github : 'https://github.com/Kasra071/Numbers_Conversion',
-        url : 'https://Numbers_Conversion.kasra071.com',
+        url : 'https://Numbers-Conversion.kasra071.com',
 
         NameFa : 'تبدیل کننده اعداد',
         descFa : 'در این صفحه میتوانید سیستم های عددی مختلف را به یکدیگر تبدیل کنید. از سیستم 2 تایی تا سیستم 36 تایی. کافیه سیستم ها و اعدادتون رو وارد کنید و تبدیل کردن رو شروع کنید !',
