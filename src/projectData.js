9e81bd4b69f3876e637b18b832869b15e8433457
Kasra071 <bitcoinsamani@yahoo.com>
2023-08-07 04:41:16 +0330

    added 2 projects

--- src/projectData.js
+++ src/projectData.js
@@ -40,5 +40,32 @@ var PersonalProjects = [
 
         NameFa : 'تبدیل کننده اعداد',
         descFa : 'در این صفحه میتوانید سیستم های عددی مختلف را به یکدیگر تبدیل کنید. از سیستم 2 تایی تا سیستم 36 تایی. کافیه سیستم ها و اعدادتون رو وارد کنید و تبدیل کردن رو شروع کنید !',
-    }
+    },
+    {
+        Name : 'Color Tools',
+        img : 'dl_k071/projects/c_T/1.png',
+        desc : 'Experience color like never before with my dynamic Color Tool! Convert HEX to RGB and vice versa effortlessly. Get creative with a random color generator, and fine-tune shades using live sliders. Preview text in black and white against your chosen color. Easily select the perfect hue with the HTML default color picker. Elevate your designs with this essential color companion! ',
+        builtWith : [
+            'HTML','CSS','JS'
+        ],
+        github : 'https://github.com/Kasra071/color-tools',
+        url : 'https://Color-Tools.kasra071.com',
+
+        NameFa : ' ابزار رنگ ها',
+        descFa : 'با این ابزار میتوانید کد های رنگی HEX را به RGB تبدیل کنید و برعکس! میتوانید رنگ های تصادفی تولید کنید . و با کم و زیاد کردن مقادیر به صورت زنده تغییرات رو ببینید. متن سفید و مشکی رو روی رنگ ببینید و بهترین رنگ رو انتخاب کنید برای کارتون!',
+    },
+    {
+        Name : 'Guess The Color Game',
+        img : 'dl_k071/projects/GCG/1.png',
+        desc : 'Test your color recognition skills! Match RGB codes to their corresponding colors, choose between HEX and RGB formats, and adjust difficulty levels. Sharpen your eye for hues and showcase your prowess. Play now!',
+        builtWith : [
+            'HTML','CSS','JS'
+        ],
+        github : 'https://github.com/Kasra071/GuessTheColorGame',
+        url : 'https://GuessTheColorGame.kasra071.com',
+
+        NameFa : 'بازی حدس زدن رنگ',
+        descFa : ' تست کنید چقدر رنگ‌ها را شناسایی می‌کنید! کدهای RGB را با رنگ‌های مربوطه مطابقت دهید، بین فرمت‌های HEX و RGB انتخاب کنید و سطح سختی را تنظیم کنید. چشم خود را برای رنگ‌ها تیز کنید و استعداد رنگ‌آمیزی‌تان را نمایش دهید. همین حالا بازی کنید!',
+    },
+  
 ]
